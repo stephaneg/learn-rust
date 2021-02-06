672a18237542380e04f81bdcac034050c44fbf82
Stephane <stephstephg@gmail.com>
2021-02-06 18:37:29 +0100

    starting tutorial

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,3 +1,21 @@
+use std::fmt;
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+impl fmt::Display for  Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "( {} {} )\n( {} {} )", self.0, self.1, self.2, self.3)
+    }
+}
+
+fn transpose(matrix: Matrix) -> Matrix {
+    let m2 = Matrix(matrix.0, matrix.2, matrix.1, matrix.3);
+    m2
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("Matrix:\n{}", matrix);
+    println!("Transpose:\n{}", transpose(matrix))
 }
